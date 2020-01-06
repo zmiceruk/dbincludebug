1357373721d4a7ec479aa227e4d369084155be03
Dzmitry.Babrovich <dzmitry.babrovich@condecosoftware.com>
2020-01-06 14:51:38 +0000

    Added data binding to the project.

--- app/src/main/java/com/example/dbbugapplication/MainActivity.kt
+++ app/src/main/java/com/example/dbbugapplication/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.example.dbbugapplication
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import androidx.appcompat.app.AppCompatActivity
+import com.example.dbbugapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val viewBinding = ActivityMainBinding.inflate(LayoutInflater.from(this))
+        viewBinding.optionBookingName.title.text = "Title from Code"
+        viewBinding.optionBookingName.editText.hint = "Hint from Code"
+
+        setContentView(viewBinding.root)
     }
 }
